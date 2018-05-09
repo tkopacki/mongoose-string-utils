dde0f368803aa31f13b3b040fb4092edc6e6cd42
Tomasz Kopacki <kopacki.tomasz@gmail.com>
2018-05-09 19:27:37 +0200

    Structure (#1)
    * all functions grouped into one structure

--- mjs_fs/api_string.js
+++ mjs_fs/api_string.js
@@ -1,42 +1,41 @@
-function string_ltrim(string) {
-    let idx = 0;
-    while(string[idx] === ' ') {
-        idx++;
-    }
-    return string.slice(idx, string.length);
-}
-
-function string_rtrim(string) {
-    let idx = string.length - 1;
-    while(string[idx] === ' ') {
-        idx--;
-    }
-    return string.slice(0, idx + 1);
-}
-
-function string_trim(string) {
-    return string_rtrim(string_ltrim(string));
-}
-
-function string_split(string, delimeter, trim) {
-    delimeter = (delimeter === undefined ? ' ' : delimeter);
-    trim = (trim === undefined ? true : trim);
-    string += delimeter;   
-    let counter = 0;
-    let buffer = "";
-    let result = [];
-    for(let idx = 0 ; idx < string.length ; idx++) {
-        if(string[idx] === delimeter) {
-            if(trim === true) {
-                buffer = string_trim(buffer);
-            }
-            if(buffer !== '') {
-                result[counter++] = buffer;
-            }
-            buffer = "";
-        } else {
-            buffer += string[idx];
+let StringUtils = {
+    ltrim: function(string) {
+        let idx = 0;
+        while(string[idx] === ' ') {
+            idx++;
         }
+        return string.slice(idx, string.length);
+    },
+    rtrim: function(string) {
+        let idx = string.length - 1;
+        while(string[idx] === ' ') {
+            idx--;
+        }
+        return string.slice(0, idx + 1);
+    },
+    trim: function(string) {
+        return StringUtils.rtrim(StringUtils.ltrim(string));
+    },
+    split: function(string, delimeter, trim) {
+        delimeter = (delimeter === undefined ? ' ' : delimeter);
+        trim = (trim === undefined ? true : trim);
+        string += delimeter;   
+        let counter = 0;
+        let buffer = "";
+        let result = [];
+        for(let idx = 0 ; idx < string.length ; idx++) {
+            if(string[idx] === delimeter) {
+                if(trim === true) {
+                    buffer = StringUtils.trim(buffer);
+                }
+                if(buffer !== '') {
+                    result[counter++] = buffer;
+                }
+                buffer = "";
+            } else {
+                buffer += string[idx];
+            }
+        }
+        return result;
     }
-    return result;
 }
