b2d6178fa358ba07a73d26c0d4763b9969202f18
Tomasz Kopacki <tomasz.kopacki@nokia.com>
2018-05-09 19:19:12 +0200

    bugs fixed

--- mjs_fs/api_string.js
+++ mjs_fs/api_string.js
@@ -1,22 +1,22 @@
 StringUtils = {
-    ltrim = function(string) {
+    ltrim: function(string) {
         let idx = 0;
         while(string[idx] === ' ') {
             idx++;
         }
         return string.slice(idx, string.length);
     },
-    rtrim = function(string) {
+    rtrim: function(string) {
         let idx = string.length - 1;
         while(string[idx] === ' ') {
             idx--;
         }
         return string.slice(0, idx + 1);
     },
-    trim = function(string) {
-        return string_rtrim(string_ltrim(string));
+    trim: function(string) {
+        return StringUtils.rtrim(StringUtils.ltrim(string));
     },
-    split = function(string, delimeter, trim) {
+    split: function(string, delimeter, trim) {
         delimeter = (delimeter === undefined ? ' ' : delimeter);
         trim = (trim === undefined ? true : trim);
         string += delimeter;   
@@ -26,7 +26,7 @@ StringUtils = {
         for(let idx = 0 ; idx < string.length ; idx++) {
             if(string[idx] === delimeter) {
                 if(trim === true) {
-                    buffer = string_trim(buffer);
+                    buffer = StringUtils.trim(buffer);
                 }
                 if(buffer !== '') {
                     result[counter++] = buffer;
